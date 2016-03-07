f5fe2ef5b3d17d132c9c6fdf27fffd877ac0d60b
Karco <karco@olan.ca>
2016-03-07 15:30:07 -0500

    Initial Commit

---
+++ smartapps/karco/karco-app-1.src/karco-app-1.groovy
@@ -0,0 +1,58 @@
+/**
+ *  Karco_app_#1
+ *
+ *  Copyright 2016 Karco
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Karco_app_#1",
+    namespace: "Karco",
+    author: "Karco",
+    description: "Training application ",
+    category: "Green Living",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    oauth: [displayName: "Karco ", displayLink: ""])
+
+
+preferences {
+	section("Choose one or more, when..."){
+		input "contact", "capability.contactSensor", title: "Contact Opens", required: false, multiple: true
+		input "contactClosed", "capability.contactSensor", title: "Contact Closes", required: false, multiple: true
+	}
+    section("Turn on this light when open") {
+        input "theswitch1", "capability.switch", required: true
+    }
+    section("Turn off this light when close") {
+        input "theswitch2", "capability.switch", required: true
+    }
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+// TODO: implement event handlers
